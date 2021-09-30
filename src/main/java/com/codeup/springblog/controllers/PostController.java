1eb956a6cd54fd0d613aaaffa43ace172cfa9244
Cierra Smith <cierra.r.smith3@gmail.com>
2021-09-30 11:30:52 -0500

    added styling

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,9 +1,11 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repos.PostRepository;
 import com.codeup.springblog.repos.UserRepository;
 import com.codeup.springblog.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -57,7 +59,8 @@ public class PostController {
             @ModelAttribute Post post
     ) {
 
-        post.setOwner(userDao.getById(1L));
+
+        post.setOwner((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 
         emailService.prepareAndSend(
                 post,
--- src/main/java/com/codeup/springblog/controllers/UsersController.java
+++ src/main/java/com/codeup/springblog/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.controllers;
 import com.codeup.springblog.models.User;
 import com.codeup.springblog.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +11,37 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class UsersController {
 
-    @Autowired
-    private UserRepository userDao;
+    private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
+
+    public UsersController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+//    @Autowired
+//    private UserRepository userDao;
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "user/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
+    // Had this in authentication controller but it wasn't working, maybe missing DAO?
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "user/login";
+    }
+
 
     @GetMapping("/user/create")
     public String createUserForm() {
